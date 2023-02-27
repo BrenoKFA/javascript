9d51645c2ce609215c28b74fc64fa4ec55404fa9
Breno Kauã <brenokaua09@gmail.com>
2023-02-27 12:00:47 -0300

    condições pt 1

---
+++ exercícios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do caro é ${vel}Km/h`)
+if(vel>60) {
+    console.log('Você ultapassou a velocidade pemitida. MULTADO!')
+} 
+console.log('Dirija sempe usando cinto de segurança!')
