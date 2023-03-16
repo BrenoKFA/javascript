5a7db1bee2a1fea4a08a7b59aaa5fbfe8574fae1
Breno Kauã <brenokaua09@gmail.com>
2023-03-16 17:40:30 -0300

    mais um pouco de requisições

--- b7web/testes/requisicoes/requisicao/script.js
+++ b7web/testes/requisicoes/requisicao/script.js
@@ -8,9 +8,42 @@ function clicou() {
         })
         // transformou a resposta em um objeto
         .then((json) => {
-            console.log(json[0].title)
+            alert(json[0].title)
         })
         // mostra a resposta já em forma de objeto
+        .catch(() => {
+            alert('Erro na requisição')
+        })
+        // é executado quando acontece algum erro
+        .finally(() => {
+            alert('FIM :)')
+        })
+        // é executado idependentemente se deu certo ou errado
+}
+
+function inserir() {
+    fetch(
+        'https://jsonplaceholder.typicode.com/posts', 
+        {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            }, // tipo de dado que vai mandar
+            body: JSON.stringify({
+                title: 'Titulo de teste',
+                body: 'Corpo de teste',
+                userId: 2
+            }) // dado que está mandando
+        })
+        // requisição com método POST
+
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            console.log(json)
+        })
 }
 
 document.querySelector('#botao').addEventListener('click', clicou)
+document.querySelector('#inserir').addEventListener('click', inserir)
